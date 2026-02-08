363ef19c52b99f32b6a7b5cccda037bf59b1a424
Taylor Turner <131205485+TaylorTurnerIT@users.noreply.github.com>
2026-02-07 23:37:01 -0600

    update immich.nix

--- containers/immich.nix
+++ containers/immich.nix
@@ -67,9 +67,8 @@ in {
         IMMICH_MACHINE_LEARNING_URL = "http://immich_machine_learning:3003";
       };
       volumes = [
-        # [CRITICAL CHANGE] Official guide mounts to /data, not /usr/src/app/upload
         "${hostUploadDir}:/data" 
-        "/etc/localtime:/etc/localtime:ro"
+        # "/etc/localtime:/etc/localtime:ro"
         "${config.sops.secrets.immich_db_password.path}:/run/secrets/immich_db_password:ro"
       ];
       dependsOn = [ "immich_redis" "immich_postgres" ];
